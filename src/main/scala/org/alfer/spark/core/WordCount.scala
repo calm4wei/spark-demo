834876e08aed10eb0a570d2915cfe538b7e3a58e
weifeng <weifeng@zqykj.com>
2017-07-21 15:22:11 +0800

    fix the issue: java.net.BindException: 无法指定被请求的地址: Service 'sparkDriver' failed after 16 retries!

--- src/main/scala/org/alfer/spark/core/WordCount.scala
+++ src/main/scala/org/alfer/spark/core/WordCount.scala
@@ -10,24 +10,29 @@ import org.apache.spark.{SparkConf, SparkContext}
 object WordCount {
 
 
-	def main(args: Array[String]) {
-		StreamingLogger.setStreamingLogLevels()
+  def main(args: Array[String]) {
+    StreamingLogger.setStreamingLogLevels()
 
-		val path = "hdfs://master01:9001/user/alfer/wc.txt"
-		val jobName = "WordCount"
-		val sparkConf = new SparkConf()
-			.setAppName(jobName)
-			.setMaster("local[4]")
-		val sc = new SparkContext(sparkConf)
+    val path = "hdfs://work82:8020/user/root/README"
+    val jobName = "WordCount"
+    val sparkConf = new SparkConf()
+      .setAppName(jobName)
+      //      .set("spark.driver.port", "9400")
+      //      .set("spark.port.maxRetries", "100")
+      //      .set("spark.ui.port", "4040")
+      .set("spark.driver.host", "localhost")
+      .setMaster("local[4]")
+    val sc = new SparkContext(sparkConf)
 
-		// 可以读入多个文件，通过正则进行匹配
-		val data = sc.textFile(path)
+    // 可以读入多个文件，通过正则进行匹配
+    val data = sc.textFile(path)
 
-		val wc = data.flatMap(l => l.trim.split(" "))
-			.map(w => (w, 1))
+    val wc = data.flatMap(l => l.trim.split(" "))
+      .map(w => (w, 1))
 
-		val wCount = wc.reduceByKey((a, b) => a + b)
-		wCount.foreach(print(_))
+    val wCount = wc.reduceByKey(_ + _)
+    //		val wCount = wc.reduceByKey((a, b) => a + b)
+    wCount.foreach(print(_))
 
-	}
+  }
 }
