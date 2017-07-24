0c444911dc919bee2d866e5c4de0e43128cc9eaf
weifeng <weifeng@zqykj.com>
2017-07-24 16:52:22 +0800

    hbase conn

---
+++ src/main/scala/org/alfer/spark/core/HBaseTest.scala
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.alfer.spark.core
+
+import java.util
+import java.util.UUID
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase._
+import org.apache.hadoop.hbase.client.{Admin, ConnectionFactory, Put, Result}
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.io.compress.Compression.Algorithm
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+object HBaseTest {
+  def main(args: Array[String]) {
+    val sparkConf = new SparkConf()
+      .setAppName("HBaseTest")
+      .setMaster("local[4]")
+    val sc = new SparkContext(sparkConf)
+    val conf = HBaseConfiguration.create()
+    conf.set("hbase.zookeeper.quorum", "172.30.6.81")
+    conf.set("hbase.zookeeper.property.clientPort", "2181")
+    conf.set("zookeeper.znode.parent", "/hbase")
+    conf.set(TableInputFormat.INPUT_TABLE, "demo01")
+
+    val tableName = "demo01"
+    // createSchemaTables(conf, "demo01")
+    // fakeData(conf, tableName, 10)
+    readData(sc, conf)
+
+
+    sc.stop()
+  }
+
+  //  def createNamespace(conf: Configuration, nameSpace: String): Unit = {
+  //    val connection = ConnectionFactory.createConnection(conf)
+  //    val admin = connection.getAdmin
+  //    val namespaceDesc = new NamespaceDescriptor("test")
+  //    admin.createNamespace(namespaceDesc)
+  //    connection.close()
+  //  }
+
+  def readData(sc: SparkContext, conf: Configuration): Unit = {
+    val hbaseRDD = sc.newAPIHadoopRDD(conf,
+      classOf[TableInputFormat],
+      classOf[ImmutableBytesWritable],
+      classOf[Result])
+    hbaseRDD.foreach(println(_))
+  }
+
+  def fakeData(conf: Configuration, tableName: String, num: Int) {
+    val connection = ConnectionFactory.createConnection(conf)
+    val hTable = connection.getTable(TableName.valueOf(tableName))
+    val putList = new util.ArrayList[Put]()
+    for (i <- 1 until (num)) {
+      val put = new Put(Bytes.toBytes(i + ""))
+      val value = UUID.randomUUID().toString.replaceAll("-", "")
+      put.addColumn(Bytes.toBytes("f"), Bytes.toBytes(i + ""), Bytes.toBytes(value))
+      putList.add(put)
+    }
+    hTable.put(putList)
+    hTable.close()
+    connection.close()
+
+  }
+
+  def createSchemaTables(conf: Configuration, tableName: String): Unit = {
+    val connection = ConnectionFactory.createConnection(conf)
+    val admin = connection.getAdmin
+    val tableDesc = new HTableDescriptor(TableName.valueOf(tableName))
+    tableDesc.addFamily(new HColumnDescriptor("f").setCompressionType(Algorithm.SNAPPY))
+    createOrOverwrite(admin, tableDesc)
+    connection.close()
+  }
+
+  def createOrOverwrite(admin: Admin, tableDesc: HTableDescriptor): Unit = {
+    if (!admin.tableExists(tableDesc.getTableName)) {
+      admin.createTable(tableDesc)
+    }
+  }
+}
--- src/main/scala/org/alfer/spark/sql/RDDRelationDemo.scala
+++ src/main/scala/org/alfer/spark/sql/RDDRelationDemo.scala
@@ -1,8 +1,38 @@
 package org.alfer.spark.sql
 
+import com.mongodb.spark.MongoSpark
+import org.alfer.spark.streaming.StreamingLogger
+import org.apache.spark.sql.{SparkSession}
+
 /**
   * Created by alfer on 7/22/17.
   */
 object RDDRelationDemo {
 
+  def main(args: Array[String]): Unit = {
+    StreamingLogger.setStreamingLogLevels()
+
+    val sparkSession = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.driver.host", "localhost")
+      .master("local[4]")
+      .config("spark.mongodb.input.uri", "mongodb://dev60/hyjj.ELPModel")
+      .getOrCreate()
+
+    //    val options = Map[String, String](
+    //      "host" -> "dev60",
+    //      "database" -> "hyjj",
+    //      "collection" -> "ELPModel"
+    //    )
+
+    val df1 = MongoSpark.load(sparkSession)
+    df1.printSchema()
+
+    val df2 = sparkSession.read.format("com.mongodb.spark.sql").load()
+    df2.show()
+    val ds3 = df2.as("ds").select("ds.creator", "ds.createTime", "ds.entities", "ds.links", "ds.graphState")
+    ds3.show()
+
+  }
 }
